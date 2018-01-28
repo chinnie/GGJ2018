283684e2303d3b1dcde4f88806d9d38be41e9f03
Dave P <dpav@popoko.com>
2018-01-27 19:18:38 -0800

    Something something changes

--- Assets/_RoboCharm/script/Robot_hoppy.cs
+++ Assets/_RoboCharm/script/Robot_hoppy.cs
@@ -1,58 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class Robot_hoppy : MonoBehaviour {
+//public class Robot_hoppy : MonoBehaviour {
 
-    [SerializeField] private bool RobotUseful = true;
-    [SerializeField] private bool isActive = true;
-    [SerializeField] private float speed;
+//    [SerializeField] private bool RobotUseful = true;
+//    [SerializeField] private bool isActive = true;
+//    [SerializeField] private float speed;
    
 
-    // Use this for initialization
-    void Start()
-    {
-        startPosition = transform.position;
+//    // Use this for initialization
+//    void Start()
+//    {
+//        startPosition = transform.position;
 
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
 
-        if (!RobotUseful)
-        {
-            //plays animation in place
-            endPosition = new Vector3(transform.position.x, startPosition.y + 2, transform.position.z);
-        }
-        else
-        {
-            endPosition = goalPosition;
-        }
-        if (isActive)
-        {
+//        if (!RobotUseful)
+//        {
+//            //plays animation in place
+//            endPosition = new Vector3(transform.position.x, startPosition.y + 2, transform.position.z);
+//        }
+//        else
+//        {
+//            endPosition = goalPosition;
+//        }
+//        if (isActive)
+//        {
 
-            //move forward
-            if (startPosition != endPosition)
-            {
-                transform.position = Vector3.Lerp(transform.position, endPosition, speed * Time.deltaTime);
-            }
+//            //move forward
+//            if (startPosition != endPosition)
+//            {
+//                transform.position = Vector3.Lerp(transform.position, endPosition, speed * Time.deltaTime);
+//            }
 
-        }
-    }
+//        }
+//    }
 
-    //When activated perform this action
-    void Activate()
-    {
-        //Push will move forward in a straigt line 
-        isActive = !isActive;
-    }
+//    //When activated perform this action
+//    void Activate()
+//    {
+//        //Push will move forward in a straigt line 
+//        isActive = !isActive;
+//    }
 
-    //Toggle useful and silly functions
-    void Toggle()
-    {
-        RobotUseful = !RobotUseful;
-        // reset EndPostion
-        endPosition = goalPosition;
-    }
-}
+//    //Toggle useful and silly functions
+//    void Toggle()
+//    {
+//        RobotUseful = !RobotUseful;
+//        // reset EndPostion
+//        endPosition = goalPosition;
+//    }
+//}
