8ec136a93f2703a31ce7fa7e8d6cb16e3a1b010d
Kam Chuen Mak <kcmak1987@gmail.com>
2018-01-28 16:06:57 -0800

    Add hatch into elevator scene

---
+++ Assets/_RoboCharm/script/openHatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using _RoboCharm.scripts;
+
+public class openHatch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+       GameObject elevator = GameObject.FindGameObjectWithTag("Elevator");
+        elevator.GetComponent<Elevator>().DisableBarrier();
+        GetComponent<Animator>().SetTrigger("Open");
+    }
+}
